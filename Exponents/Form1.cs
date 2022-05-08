21059e6edba6c1ae5254a61ad35a6a793287923c
cmunoz1753 <103385435+cmunoz1753@users.noreply.github.com>
2022-05-08 15:58:48 -0400

    Added teh square and cube function with 2 public methods.

--- Exponents/Form1.cs
+++ Exponents/Form1.cs
@@ -28,32 +28,40 @@ namespace Exponents
         private void xGoButton_Click(object sender, EventArgs e)
         {
             // STORAGE
-            
+            int num;
+
+
 
             // INPUT
-           
+            num = Convert.ToInt32(uxInputTextBox.Text);
+
             // PROCESS
             /* calculate the square of the number and add it to the output - 
              * do this with a method call that allows you to pass the number
              * and then receive the answer */
-
+            uxOutputLabel.Text = "Square is " + Square(num).ToString();
+            
             /* calculate the cube of the number and add it to the output - 
              * do this by creating a methd as in the squaring example above */
+            uxOutputLabel.Text += "\nCube is " + Cube(num).ToString();
 
             // OUTPUT
             // set the output to visible
-
+            uxOutputLabel.Visible = true;
             // turn the Go button off
+            uxGoButton.Enabled = false;
         }
 
         public int Square(int num)
         {
             // we will create this code in class
+            return num * num;
         }
 
         public int Cube(int num)
         {
             // we will create this code in class
+            return num * num * num;
         }
 
         private void xResetButton_Click(object sender, EventArgs e)
